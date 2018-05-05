a31cb210104377df63984ab8f497e34076c932c5
Ugarking <sylvesterwalt@yahoo.com.ph>
2018-05-05 19:37:26 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,10 +20,10 @@ void displayMat() //This program displays the matrix entered by user.
     int elemno = 0;
     do
     {
-    cout << data[elemno] << " \t";
-    elemno++;
-    if (elemno % col == 0) //new line when k reaches the number "col"
-      cout << endl;
+      cout << data[elemno] << " \t";
+      elemno++;
+      if (elemno % col == 0) //new line when k reaches the number "col"
+        cout << endl;
     }
     while (elemno < size);
     cout << endl;
@@ -38,16 +38,12 @@ bool noSol(int colPos, int countCol) //Checks if Matrix has No Sol'n
     {
       countCol++;
       if (noSol(i+1, countCol) == true)
-      {
         return true;
-      }
       else
       {
         countCol = 0;
         if (i % col != 0)
-        {
           return false;
-        }
       }
     }
     else if (data[i-1] == 0 && countCol == col)
@@ -58,13 +54,9 @@ bool noSol(int colPos, int countCol) //Checks if Matrix has No Sol'n
     else if (data[i] != 0 && i % col != 0)
     {
       if (countCol+1 == col)
-      {
         return true;
-      }
       else
-      {
         return false;
-      }
     }
   }
   return false;
@@ -73,13 +65,9 @@ bool noSol(int colPos, int countCol) //Checks if Matrix has No Sol'n
 bool multSol() //Checks if Matrix Has Multiple Sol'n
 { //This happens when the #of rows doesnt match the #of columns-1
   if (row  != col-1)
-  {
     return true;
-  }
   else
-  {
     return false;
-  }
 }
 
 void output() //Outputs the solution to the matrix
@@ -91,47 +79,56 @@ void output() //Outputs the solution to the matrix
 
 void solve() //Uses back substitution to get the answers to the matrix
 {
-  cout << "Solving...\n";
+  if (choice == 0)
+    cout << "Solving...\n";
   double d; //denominator
   double t[row]; //numerator terms
-  for(int i=1; i<=row; i++)
+  for(int i=1; i<=row; i++) //goes thru each row starting with last
   {
-    t[0] = -data[(size-1)-((i-1)*col)];
-    d = data[(size-1)-((i-1)*col)-i];
+    /*size-1 indicates we get the data
+    with reference from the last term*/
+    t[0] = -data[(size-1)-((i-1)*col)]; //right-side coefficient
+    d = data[(size-1)-((i-1)*col)-i]; //main diagonal coefficient
     /*var was initialized to zero so that
     it's null when it's not needed in the terms*/
     /*var will be given a value for the
     next iterations where it's needed*/
-    for(int j=1; j<row; j++)
+    for(int j=1; j<row; j++) //goes thru each col of i row
     {
+      /*multiplies the previous solved vars with
+      their coefficients in i row*/
       t[j] = var[row-j]*data[(size-1)-((i-1)*col)-j];
     }
     for(int j=0; j<row; j++)
-    {
-      var[row-i] -= t[j];
-    }
-    var[row-i] /= d;
+      var[row-i] -= t[j]; //subtracts each terms
+    var[row-i] /= d; //divides by the var's coefficient
   }
 }
 
 void eliminate(int c) //Performs The Gauss method of elimination
 {
+  /*c here tells which diagonal pivot we are located as the reference
   /* This function eliminates the elements under the pivot*/
   if (choice == 0)
-  {
     cout << "Eliminating...\n";
-  }
   double f; //pivot factor
   int pivot = c*col+c; //pivot's array address
+  /*goes thru each row where c limits the number of iterations
+  as the pivot goes to the next row*/
   for (int i=1; (i+c)<row; i++)
   {
+    /*divides the negative of the data under pivot's column 
+    by the pivot; this is the factor for making the pivot column zero
+    where Af + B = 0 => f = -B/A*/
     f=-data[(c+i)*col+c]/data[pivot];
-    data[(i+c)*col+c]=0;
-    //sets terms in the same column as the pivot to zero
-    for (int j=1+c; j<col ;j++)
+    data[(i+c)*col+c]=0; //sets pivot column to zero
+    for (int j=1+c; j<col ;j++) //goes thru each col of i row
     {
+      /*given the form Af + B = Anext, the row terms
+      are adjusted accordingly with the pivot factor
+      where A is the term above the term being adjusted
+      which is B*/
       data[(c+i)*col+j]+=f*data[j+c*col];
-      //adjusts each term accordingly by subtracting to the data
     }
   }
   displayMat(); //Shows the user the adjusted matrix
@@ -169,20 +166,22 @@ void setPivot(int c) //This function rearranges the pivot
   //compares old and new addresses
   if (dpivot == itmp)
   {
-    if (choice == 0)
-    {
+    if (choice == 0) //if the same, no pivot needed
       cout << "No Pivot Needed\n\n";
-    }
-
     return;
   }
   else
+  {
+    /*starts with dpivot "the old pivot" location until col is reached.
+    itmp is the new pivot location where j increments as the code
+    goes thru the col of the pivot row*/
     for (int j=0, k = dpivot; k<col; j++, k++)
     {
       dtmp = data[k]; //storing old pivot row
       data[k] = data[itmp+j]; //equate old pivot row to new pivot row
       data[itmp+j] = dtmp; //equate new pivot row to old pivot row
     }
+  }
   displayMat();
   return;
 }
@@ -304,39 +303,39 @@ int pow10(double a, int n) //multiplies num by 10^n
 
 double sigfig(double a, int n) //rounds number to specified amt of sf
 {
-  int flaga = 1;
-  int flagb = 1;
-  int b1,b2;
+  int flaga = 1; //flag for a<1 doubles
+  int flagb = 1; //flag for a>1 doubles
+  int b1,b2; //used to compare rounding digit and its lower digit
   double temp = a;
   a = dabs(a);
 
   //set to form x.xxx
-  while(a>1) //counts # of digits before the decimal point
+  while(a>1) //sets double like 12.3 to 1.23
   {
     a/=10;
     flaga++;
   };
-  while(a<1) //counts the # of digits after the decimal point
+  while(a<1) //sets double like 0.123 to 1.23
   {
     a*=10;
     flagb++;
   };
-  b1 = pow10(a,n);
-  b2 = pow10(a,n-1);
+  b1 = pow10(a,n); //acquires the significant figure last digit
+  b2 = pow10(a,n-1); //acquires the digit to the right of that
 
-  if((b1%b2)>=5)
-  {
-    if (temp>=0)
+  /*does Half Round Up Method. For example
+  1.4->1, 1.5->2, 1.6->2, -1.4->-1, -1.5->-1, -1.6->-2*/
+  if((b1%b2)>=5 && temp>=0)  
       b2++;
-    else
-      b2--;
-  }
+  else if ((b1%b2)>5 && temp<0)
+      b2++;
 
-  a=b2;
+  a=b2; //stores int to double to apply decimals
 
-  for(int i=1; i<n; i++)
+  for(int i=1; i<n; i++) //reverts back by n (sigfig) times
     a /= 10;
 
+  //reverts further more from x.xxx form
   while(flaga>1)
   {
     a*=10;
@@ -348,7 +347,7 @@ double sigfig(double a, int n) //rounds number to specified amt of sf
     flagb--;
   };
 
-  if (temp>=0)
+  if (temp>=0) //returns with the original's sign
     return a;
   else
     return -a;
